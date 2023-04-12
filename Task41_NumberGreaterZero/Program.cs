fe7e2fe7f7834956bcdcf99a577585c714e86c84
Natali <nbyka@mail.ru>
2023-04-12 22:43:41 +0300

    NumberGreaterZero

---
+++ Task41_NumberGreaterZero/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+
+int [] array = {1, -7, 568, 89, 223, -10, 120, 5, -35, -91, 1023};
+
+int Solution(int [] array)
+{
+    int result = 0; 
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+        {
+            result += 1;          
+        }        
+    }    
+    Console.WriteLine(result);
+    return result;    
+}
+
+Solution(array);
